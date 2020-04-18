ef1a1396e3c046c42f1cde473ab7a1248ba55ee9
sunlight <49097916+sssssssssssssssssa@users.noreply.github.com>
2020-04-18 22:16:20 +0800

    身份证格式验证和最后一位有效性验证

--- 身份证合法验证/身份证合法验证/Form1.Designer.cs
+++ 身份证合法验证/身份证合法验证/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(345, 278);
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(336, 333);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "判断";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,26 +47,38 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(336, 106);
+            this.textBox1.Location = new System.Drawing.Point(293, 106);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.Size = new System.Drawing.Size(258, 25);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "输入身份证号";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(355, 196);
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(332, 191);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.Size = new System.Drawing.Size(154, 24);
             this.label1.TabIndex = 2;
-            this.label1.Text = "判断结果";
+            this.label1.Text = "格式验证结果";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(289, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "身份证最后一位有效性";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -80,6 +94,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- 身份证合法验证/身份证合法验证/Form1.cs
+++ 身份证合法验证/身份证合法验证/Form1.cs
@@ -17,9 +17,110 @@ namespace 身份证合法验证
             InitializeComponent();
         }
 
+        string ID;      //定义身份证号
+
         private void button1_Click(object sender, EventArgs e)
         {
+            ID = this.textBox1.Text;    //读取身份证输入
+            //this.label1.Text = ID;    //测试输入
+
+            bool result1 = formatCheck(ID);
+            if (result1)
+            {
+                label1.Text = "身份证格式正确";
+            }
+            else
+            {
+                label1.Text = "身份证格式不正确";
+            }
+            
+            bool result2 = CheckChinaIDCardNumberFormat(ID);           
+            if (result2)
+            {
+                label2.Text = "身份证最后一位有效位合法";
+            }
+            else
+            {
+                label2.Text = "身份证最后一位有效位不合法";
+            }
+        }
+
+
+        public static bool formatCheck(string idCardNumber)
+        {
+            string idNumber = idCardNumber;
+            bool result = true;     //定义判断结果
+
+            System.Text.RegularExpressions.Regex rgX = new System.Text.RegularExpressions.Regex(@"^\d{17}X$");
+            System.Text.RegularExpressions.Regex rgN = new System.Text.RegularExpressions.Regex(@"^\d{18}$");
+            System.Text.RegularExpressions.Match mc1 = rgX.Match(idNumber);
+            System.Text.RegularExpressions.Match mc2 = rgN.Match(idNumber);
+
+            if (!mc1.Success && !mc2.Success)   //两种格式都不符合
+            {
+                result = false;
+            }
+
+            return result;
 
         }
+
+        public static bool CheckChinaIDCardNumberFormat(string idCardNumber)
+        {
+            string idNumber = idCardNumber;
+            bool result = true;     //定义判断结果
+            try
+            {
+                if (idNumber.Length != 18)  //输入身份证长度
+                {
+                    return false;
+                }
+
+
+                long n = 0;
+                if (long.TryParse(idNumber.Remove(17), out n) == false
+                    || n < Math.Pow(10, 16) || long.TryParse(idNumber.Replace('x', '0').Replace('X', '0'), out n) == false)
+                {
+                    return false;   //数字验证  
+                }
+
+                string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+                if (address.IndexOf(idNumber.Remove(2)) == -1)
+                {
+                    return false;   //省份验证  
+                }
+
+                string birth = idNumber.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+                DateTime time = new DateTime();
+                if (DateTime.TryParse(birth, out time) == false)
+                {
+                    return false;   //生日验证  
+                }
+                string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+                string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+                char[] Ai = idNumber.Remove(17).ToCharArray();
+                int sum = 0;
+                for (int i = 0; i < 17; i++)
+                {
+                    sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+                }
+                int y = -1;
+                Math.DivRem(sum, 11, out y);
+                if (arrVarifyCode[y] != idNumber.Substring(17, 1).ToLower())
+                {
+                    return false;   //校验码验证  
+                }
+                return true;//符合GB11643-1999标准 
+
+            }
+            catch (Exception ex)
+            {
+                //logger.Error("CheckChinaIDCardNumber 身份证校验失败  号码:" + idNumber, ex);
+                result = false;
+            }
+            return result;
+        }
+
+
     }
 }
